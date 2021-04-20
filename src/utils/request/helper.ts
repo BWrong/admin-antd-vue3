d7e261b4db6fe7f2d76e0930ac5c25f74c32f1cf
bwrong <ibwrong@foxmail.com>
2021-04-20 16:19:16 +0800

    fix: 优化request代码

--- src/utils/request/helper.ts
+++ src/utils/request/helper.ts
@@ -1,24 +1,38 @@
-import { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import Cookie from 'js-cookie';
 import appConfig from '@/config';
 import auth from '@/api/auth';
 import { computedExpires } from '@/utils/token';
+interface IRefreshToken {
+  (token: string | undefined): void;
+  refreshDoing?: boolean;
+}
+let requestCache: Map<string, boolean> = new Map(); // 请求暂存列表，列表中的请求会被取消
 /**
  * 刷新token
  * @param {string} refreshToken
  */
-export function handleRefreshToken(refreshToken: string | undefined) {
-  return new Promise((resolve, reject) => {
-    auth
-      .refreshToken({ refresh_token: refreshToken })
-      .then((res: any) => {
-        Cookie.set(appConfig.tokenKey, res.access_token);
-        Cookie.set(appConfig.refreshTokenKey, res.refresh_token);
-        Cookie.set(appConfig.tokenExpiresKey, String(computedExpires(res.data.expires_in)));
-        resolve(res);
-      })
-      .catch(reject);
-  });
+export const handleRefreshToken: IRefreshToken = (refreshToken) => {
+  if (handleRefreshToken.refreshDoing) return;
+  handleRefreshToken.refreshDoing = true; // 加锁，防止重复刷新
+  auth
+    .refreshToken({ refresh_token: refreshToken })
+    .then((res: any) => {
+      Cookie.set(appConfig.tokenKey, res.access_token);
+      Cookie.set(appConfig.refreshTokenKey, res.refresh_token);
+      Cookie.set(appConfig.tokenExpiresKey, String(computedExpires(res.data.expires_in)));
+    })
+    .finally(() => {
+      handleRefreshToken.refreshDoing = false;
+    });
+};
+/**
+ * 生成cancelToken
+ * @param config
+ * @returns
+ */
+export function ganerCancelToken(config: AxiosRequestConfig) {
+  return new axios.CancelToken((cancel) => addRequest(config, cancel));
 }
 /**
  * 生成存储的key
@@ -30,21 +44,19 @@ export function generateKey({ method, url, params }: AxiosRequestConfig) {
 }
 /**
  * 添加请求到暂存列表
- * @param {object} reqList - 请求缓存列表
  * @param {object} config 当前请求配置
  * @param {function} cancel - 请求中断函数
  */
-export function addRequest(reqList: Map<string, boolean>, config: AxiosRequestConfig, cancel: any) {
+export function addRequest(config: AxiosRequestConfig, cancel: any) {
   const key = generateKey(config);
-  reqList.has(key) ? cancel('请求被取消,config:', config) : reqList.set(key, true);
+  requestCache.has(key) ? cancel('请求被取消,config:', config) : requestCache.set(key, true);
 }
 /**
  * 从暂存列表删除请求，即释放拦截
- * @param {object} reqList 全部请求列表
  * @param {object} config 当前请求配置
  * @param {number} time 阻止时间
  */
-export function removeRequest(reqList: Map<string, boolean>, config: AxiosRequestConfig, time = 500) {
+export function removeRequest(config: AxiosRequestConfig, time = 500) {
   const key = generateKey(config);
-  setTimeout(() => reqList.delete(key), time);
+  setTimeout(() => requestCache.delete(key), time);
 }
--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -4,7 +4,7 @@
  * @Last Modified by: wangwenbing
  * @Last Modified time: 2019-05-24 13:53:33
  */
-import axios, { AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse, IResponseData } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, IResponseData } from 'axios';
 import Cookie from 'js-cookie';
 // import Qs from 'qs';
 import message from './message';
@@ -16,8 +16,6 @@ import { handleRefreshToken, removeRequest, addRequest } from './helper';
 const { NODE_ENV, VUE_APP_API_HOST, VUE_APP_API_PREFIX } = process.env;
 const baseURL = NODE_ENV === 'production' ? VUE_APP_API_HOST : VUE_APP_API_PREFIX;
 
-let refreshDoing = false; // 刷新token加锁
-let requestCache: Map<string, boolean> = new Map(); // 请求暂存列表，列表中的请求会被取消
 const request = axios.create({
   timeout: 30000,
   baseURL,
@@ -33,17 +31,15 @@ const request = axios.create({
 request.interceptors.request.use(
   (config) => {
     // 设置cancelToken对象，阻止重复请求。当上个请求未完成时，相同的请求不会进行
-    //  config.cancelToken = new axios.CancelToken((cancel) => addRequest(requestCache, config, cancel));
+    //  config.cancelToken = ganerCancelToken(config);
     // 处理token
     const token = getToken();
     if (!token) return config;
     config.headers['Authorization'] = `${appConfig.tokenPrefix} ${token}`;
     // token即将过期，刷新token
     // const tokenExpires = Cookie.get(appConfig.tokenExpiresKey);
-    // if (Number(tokenExpires) <= Date.now() && !config.isNotRefreshToken && !refreshDoing) {
-    //   refreshDoing = true; // 加锁，防止重复刷新
-    //   const refreshToken = Cookie.get(appConfig.refreshTokenKey);
-    //   handleRefreshToken(refreshToken).then(() => (refreshDoing = false));
+    // if (Number(tokenExpires) <= Date.now() && !config.isNotRefreshToken) {
+    //   handleRefreshToken(Cookie.get(appConfig.refreshTokenKey));
     // }
     return config;
   },
@@ -57,7 +53,7 @@ request.interceptors.response.use(
       return Promise.resolve(data);
     }
     // 相同请求不得在短时间内重复发送
-    removeRequest(requestCache, config);
+    // removeRequest(config);
     if (data.code === 200) {
       // if (['post', 'delete', 'put'].includes(config?.method as string) && !config.isNotTips) {
       //   data.message && message.success(data.message);
@@ -73,7 +69,7 @@ request.interceptors.response.use(
     message.destroy();
     if (axios.isCancel(error)) return Promise.reject(error);
     // 相同请求不得在短时间内重复发送
-    removeRequest(requestCache, error.config);
+    removeRequest(error.config);
     if (error.response) {
       if (error.response.status === 401) {
         message.destroy();
