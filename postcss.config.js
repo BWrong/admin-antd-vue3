ce7f359d2003c673b6f795219421fa2bc5311ea2
bwrong <ibwrong@foxmail.com>
2021-05-18 15:28:01 +0800

    feat: 多分辨率适配

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-pxtorem')({
+      rootValue: 16, // 根元素字体大小
+      unitPrecisio: 16, // 转换单位
+      propList: ['*', '!border*', '!box-shadow'], // 需要转换的规则
+      blackList: /^global-.+/ // 忽略的选择器
+      // exclude: /node_modules/i // 排除的文件
+    })
+  ]
 };
