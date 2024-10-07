6d08cef249a6622d507322d9ba04c67b180db426
wangwenbing <11535041@qq.com>
2024-10-07 19:40:38 +0800

    build: 更新eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,13 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import globals from 'globals';
-import pluginVue from 'eslint-plugin-vue';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import autoImport from './.eslintrc-auto-import.json' assert { type: 'json' };
 import unocssFlat from '@unocss/eslint-config/flat';
+import prettierConfig from '@vue/eslint-config-prettier';
 import pluginSimpleImport from 'eslint-plugin-simple-import-sort';
-// import nodePlugin from "eslint-plugin-n"
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname });
+import autoImport from './.eslintrc-auto-import.json' assert { type: 'json' };
+// import nodePlugin from "eslint-plugin-n"
 
 export default [
   {
@@ -26,48 +21,7 @@ export default [
       'src/**/*.tsx',
       'src/**/*.cts',
       'src/**/*.mts'
-    ]
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: {
-        ...globals.browser,
-        ...autoImport.globals,
-        Iconfont: true,
-        __APP_CONFIG__: true
-      }
-    }
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/strongly-recommended'],
-  {
-    files: ['src/**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser,
-        jsx: true
-        // vueFeatures: {}
-      }
-    }
-  },
-  ...compat.extends('@vue/eslint-config-prettier'),
-  unocssFlat,
-  {
-    plugins: {
-      // 'nodePlugin':nodePlugin,
-      'simple-import-sort': pluginSimpleImport
-    },
-    rules: {
-      'simple-import-sort/imports': ['warn', { groups: [['^node:'], ['^@?\\w'], ['^'], ['^\\.'], ['^\\u0000']] }],
-      'simple-import-sort/exports': 'warn',
-      '@typescript-eslint/no-explicit-any': 1,
-      '@typescript-eslint/no-unused-expressions': 0,
-      'vue/no-setup-props-destructure': 0,
-      'vue/no-dupe-keys': 0,
-      'vue/multi-word-component-names': 0
-    },
+    ],
     ignores: [
       'vite.config.*',
       'vue.config.*',
@@ -120,5 +74,46 @@ export default [
       'external/*',
       'src/config/theme.ts'
     ]
-  }
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...autoImport.globals,
+        Iconfont: true,
+        __APP_CONFIG__: true
+      }
+    }
+  },
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        jsx: true
+        // vueFeatures: {}
+      }
+    }
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/strongly-recommended'],
+  {
+    plugins: {
+      // 'nodePlugin':nodePlugin,
+      'simple-import-sort': pluginSimpleImport
+    },
+    rules: {
+      'simple-import-sort/imports': ['warn', { groups: [['^node:'], ['^@?\\w'], ['^'], ['^\\.'], ['^\\u0000']] }],
+      'simple-import-sort/exports': 'warn',
+      '@typescript-eslint/no-explicit-any': 1,
+      '@typescript-eslint/no-unused-expressions': 0,
+      'vue/no-setup-props-destructure': 0,
+      'vue/no-dupe-keys': 0,
+      'vue/multi-word-component-names': 0
+    }
+  },
+  unocssFlat,
+  prettierConfig
 ];
